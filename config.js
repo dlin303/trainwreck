70adf5c8b68c680810b4f395cdf13b582bbdfe5c
Vlad Burca <vlad@meetup.com>
2016-02-18 15:23:28 -0500

    Add new tokens

--- config.js
+++ config.js
@@ -3,7 +3,9 @@
 //Secret stuffs here. like access tokens and api keys
 
 const Secrets = {
-  WIT_ACCESS_TOKEN: process.env.WIT_ACCESS_TOKEN
+  WIT_ACCESS_TOKEN: process.env.WIT_ACCESS_TOKEN,
+  TWILIO_AUTH_TOKEN: process.env.TWILLIO_AUTH_TOKEN,
+  TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID
 };
 
 module.exports = Secrets;
