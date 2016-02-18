1048d836e896cebceb9faac013e75cca5b9d1a1c
Daron <daronlin303@gmail.com>
2016-02-18 12:11:51 -0500

    install require-promise

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 'use strict';
 
+//3rd party
 const express = require('express');
 const app = express();
+const rp = require('request-promise');
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send("Sup yo");
+  rp('http://www.google.com')
+    .then(response => res.send(response))
+    .catch(err => res.send(err));
 });
 
 app.listen(port, () => {
