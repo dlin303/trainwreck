116b2d1fffc4f2bc630bb9693c6f6ac91f6c1c3b
Calvin Hu <fivepercentjuice@gmail.com>
2016-02-19 16:58:04 -0500

    better time

--- db.js
+++ db.js
@@ -30,7 +30,7 @@ const upsertUserInfo = function(number, lastEventId, lastEventName, lastEventTim
           userInfo.lastEventName = lastEventName;
         }
         if (lastEventTime !== undefined) {
-          userInfo.lastEventTime = new Date(lastEventTime).toString();
+          userInfo.lastEventTime = new Date(lastEventTime.getDate()).toString();
         }
         if (lastGroupId !== undefined) {
           userInfo.lastGroupId = lastGroupId;
