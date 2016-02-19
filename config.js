265d8d2be448bb7a66b66262a75011d47b41ac35
Calvin Hu <fivepercentjuice@gmail.com>
2016-02-18 19:49:04 -0500

    meetup service

--- config.js
+++ config.js
@@ -3,7 +3,8 @@
 //Secret stuffs here. like access tokens and api keys
 
 const Secrets = {
-  WIT_ACCESS_TOKEN: process.env.WIT_ACCESS_TOKEN
+  WIT_ACCESS_TOKEN: process.env.WIT_ACCESS_TOKEN,
+  MEETUP_TOKEN: process.env.MEETUP_TOKEN
 };
 
 module.exports = Secrets;
---
+++ services/meetupService.js
@@ -0,0 +1,21 @@
+const MEETUP_ACCESS_TOKEN = require('../config').MEETUP_TOKEN;
+
+const rp = require('request-promise');
+
+const meetupService = {
+  getIntent: (text) => {
+    var options = {
+      uri: 'https://api.meetup.com/find/groups',
+      qs: {
+          key: MEETUP_ACCESS_TOKEN,
+          sign: 'true',
+          'photo-host': 'public',
+          page: '20'
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+    return rp(options).promise();
+  }
+}
+
+module.exports = meetupService;
